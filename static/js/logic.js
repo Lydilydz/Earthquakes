a67da1940daf49e77f9576e7a797ea20017412a8
vabigdatamover <vabigdatamover@gmail.com>
2019-11-16 13:40:45 -0500

    add new files

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,21 +10,26 @@ var queryVolcanoUrl = "https://data.humdata.org/dataset/a60ac839-920d-435a-bf7d-
 
 var oilFieldsJSON = "https://raw.githubusercontent.com/carnegieendowment/oil-climate-index-2/master/app/assets/data/oilfields.geojson"
 
+var clustersJSON = "https://raw.githubusercontent.com/Lydilydz/megaproject_GOLD/master/Supplemental_Code/Output/earthquakes-dbscan-cluster-processed_geoJSON_fmt.json"
+
 
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl, function (data) {
   // Perform a GET request to the query URL
-  d3.json(platesJSON, function(data2) {
+  d3.json(platesJSON, function (data2) {
     // Perform a GET request to the query URL
-    d3.json(queryVolcanoUrl, function(data3) {
+    d3.json(queryVolcanoUrl, function (data3) {
       // Perform a GET request to the query URL
-      d3.json(oilFieldsJSON, function(data4) {
+      d3.json(oilFieldsJSON, function (data4) {
         // Perform a GET request to the query URL
-        createFeatures(data.features, data2.features, data3.features, data4.features);
+        d3.json(clustersJSON, function (data5) {
+          // Perform a GET request to the query URL
+          createFeatures(data.features, data2.features, data3.features, data4.features, data5.features);
         })
       })
     })
+  })
 });
 
 // // Perform a GET request to the query URL
@@ -34,18 +39,21 @@ d3.json(queryUrl, function(data) {
 // });
 
 //return color based on value
-  function getColor(x) {
-    return x > 5 ? "#f40202" :
-           x > 4 ? "#f45f02" :
-           x > 3 ? "#f49702" :
-           x > 2 ? "#F4bc02" :
-           x > 1 ? "#d8f402" :
-           x > 0 ? "#93f402" :
-                "#FFEDA0";
-  }
+function getColor(x) {
+  return x > 8 ? "#0202f4" :
+    x > 7 ? "#9302f4" :
+      x > 6 ? "#f402f4" :
+        x > 5 ? "#f40202" :
+          x > 4 ? "#f45f02" :
+            x > 3 ? "#f49702" :
+              x > 2 ? "#F4bc02" :
+                x > 1 ? "#d8f402" :
+                  x > 0 ? "#93f402" :
+                    "#FFEDA0";
+}
 
 
-function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData) {
+function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData, clusterData) {
 
   // Give each feature a popup describing the place and time of the earthquake
   function onEachFeature(feature, layer) {
@@ -54,6 +62,15 @@ function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData) {
       "</h3><hr><p>Magnitude: " + feature.properties.mag + "</p>");
   }
 
+  // Popup for Cluster points
+  function onEachFeatureC(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.city +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" +
+      "</h3><hr><p>Magnitude: " + feature.properties.Magnitude + "</p>");
+  }
+
+
+  // Popup for Volcanos
   function onEachFeatureV(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.V_Name +
       "</h3><hr><p>Country: " + feature.properties.Country + "</p>" +
@@ -67,9 +84,9 @@ function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData) {
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature,
     pointToLayer: function (feature, latlng) {
-  
+
       var geojsonMarkerOptions = {
-        radius: +feature.properties.mag*4,
+        radius: +feature.properties.mag * 4,
         fillColor: getColor(feature.properties.mag),
         color: "black",
         weight: 1,
@@ -78,13 +95,13 @@ function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData) {
       };
       return L.circleMarker(latlng, geojsonMarkerOptions);
     }
-    
+
   });
 
   var volcanos = L.geoJSON(volcanoData, {
     onEachFeature: onEachFeatureV,
     pointToLayer: function (feature, latlng) {
-        var geojsonMarkerOptions = {
+      var geojsonMarkerOptions = {
         radius: 3,
         fillColor: "black",
         color: "red",
@@ -97,16 +114,34 @@ function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData) {
 
   });
 
-  // console.log(volcanos);
+
+  var clusters = L.geoJSON(clusterData, {
+    onEachFeature: onEachFeatureC,
+    pointToLayer: function (feature, latlng) {
+      var geojsonMarkerOptions = {
+        radius: +feature.properties.Magnitude * 1,
+        fillColor: getColor(feature.properties.Magnitude),
+        color: "yellow",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      };
+      return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+
+  });
+
+
+  console.log(clusters);
 
   var plates = L.geoJson(plateData, {
-    style: function(){
+    style: function () {
       return {
-          color:"orange",
-          fillColor: "white",
-          fillOpacity:0
+        color: "orange",
+        fillColor: "white",
+        fillOpacity: 0
       }
-    }, 
+    },
     onEachFeature: function (feature, layer) {
       layer.bindPopup("<h3>" + feature.properties.PlateName + "</h3>");
     }
@@ -115,31 +150,35 @@ function createFeatures(earthquakeData, plateData, volcanoData, oilfieldData) {
   // console.log(plates);
 
   var oilfields = L.geoJson(oilfieldData, {
-    style: function(){
+    style: function () {
       return {
-          color:"blue",
-          fillColor: "white",
-          fillOpacity:0
+        color: "blue",
+        fillColor: "white",
+        fillOpacity: 0
       }
-    }, 
+    },
     onEachFeature: function (feature, layer) {
       layer.bindPopup("<h3>" + feature.properties.Field_Name + "</h3>");
     }
   });
 
 
+
+
   // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes, plates, volcanos, oilfields);
-  
+  createMap(earthquakes, plates, volcanos, oilfields, clusters);
+
 }
 
-function createMap(earthquakes, plates, volcanos, oilfields) {
+
+
+function createMap(earthquakes, plates, volcanos, oilfields, clusters) {
 
   // Define streetmap and darkmap layers
-//  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-//    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
-//    "T6YbdDixkOBWH_k9GbS8JQ");
-	
+  //  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+  //    "access_token=pk.eyJ1Ijoia2pnMzEwIiwiYSI6ImNpdGRjbWhxdjAwNG0yb3A5b21jOXluZTUifQ." +
+  //    "T6YbdDixkOBWH_k9GbS8JQ");
+
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -156,22 +195,22 @@ function createMap(earthquakes, plates, volcanos, oilfields) {
   });
 
   var Esri_OceanBasemap = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}', {
-	attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-	maxZoom: 13
-});
+    attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+    maxZoom: 13
+  });
 
-var JusticeMap_income = L.tileLayer('http://www.justicemap.org/tile/{size}/income/{z}/{x}/{y}.png', {
-	attribution: '<a href="http://www.justicemap.org/terms.php">Justice Map</a>',
-	size: 'county',
-	opacity: .5,
-	bounds: [[14, -180], [72, -56]]
-});
+  var JusticeMap_income = L.tileLayer('http://www.justicemap.org/tile/{size}/income/{z}/{x}/{y}.png', {
+    attribution: '<a href="http://www.justicemap.org/terms.php">Justice Map</a>',
+    size: 'county',
+    opacity: .5,
+    bounds: [[14, -180], [72, -56]]
+  });
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
     "Street Map": streetmap,
     "Satellite Map": satmap,
-	"Ocean Basemap": Esri_OceanBasemap
+    "Ocean Basemap": Esri_OceanBasemap
   };
 
   // Create overlay object to hold our overlay layer
@@ -180,7 +219,8 @@ var JusticeMap_income = L.tileLayer('http://www.justicemap.org/tile/{size}/incom
     Plates: plates,
     Volcanos: volcanos,
     Oil_Fields: oilfields,
-	"Income": JusticeMap_income
+    Clustering: clusters,
+    "Income": JusticeMap_income
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -192,7 +232,7 @@ var JusticeMap_income = L.tileLayer('http://www.justicemap.org/tile/{size}/incom
 
     timeDimension: true,
     timeDimensionOptions: {
-      timeInterval : "P1W/today",
+      timeInterval: "P1W/today",
       period: "P2D",
       autoPlay: true
     },
@@ -202,61 +242,62 @@ var JusticeMap_income = L.tileLayer('http://www.justicemap.org/tile/{size}/incom
       autoPlay: true
     },
 
-    
+
+    // Code to set which data to display -- layers: [streetmap, earthquakes, plates, volcanos, clusters, oilfields]});   
     layers: [streetmap, earthquakes, plates, volcanos, oilfields]
   });
 
-// Pass our map layers into our layer control
-// Add the layer control to the map
-L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+  // Pass our map layers into our layer control
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 
-//add time timeDimension
-//based on example from: http://jsfiddle.net/bielfrontera/5afucs89/
-L.TimeDimension.Layer.GeoJson.GeometryCollection = L.TimeDimension.Layer.GeoJson.extend({
-  // Do not modify features. Just return the feature if it intersects the time interval
-  _getFeatureBetweenDates: function(feature, minTime, maxTime) {
-    var time = new Date(feature.properties.time);
+  //add time timeDimension
+  //based on example from: http://jsfiddle.net/bielfrontera/5afucs89/
+  L.TimeDimension.Layer.GeoJson.GeometryCollection = L.TimeDimension.Layer.GeoJson.extend({
+    // Do not modify features. Just return the feature if it intersects the time interval
+    _getFeatureBetweenDates: function (feature, minTime, maxTime) {
+      var time = new Date(feature.properties.time);
       if (time > maxTime || time < minTime) {
-          return null;
+        return null;
       }
       return feature;
-  }
-});
-var timeLayer = L.timeDimension.layer.geoJson.geometryCollection = function(layer, options) {
-  return new L.TimeDimension.Layer.GeoJson.GeometryCollection(layer, options);
-};
+    }
+  });
+  var timeLayer = L.timeDimension.layer.geoJson.geometryCollection = function (layer, options) {
+    return new L.TimeDimension.Layer.GeoJson.GeometryCollection(layer, options);
+  };
 
 
 
-//L.timeDimension.layer.geoJson(layer).addTo(myMap);
-geoJsonTimeLayer = L.timeDimension.layer.geoJson.geometryCollection(earthquakes, {
-  updateTimeDimension: true,
-  updateTimeDimensionMode: 'replace',
-  duration: 'PT1H',
-}).addTo(myMap);
+  //L.timeDimension.layer.geoJson(layer).addTo(myMap);
+  geoJsonTimeLayer = L.timeDimension.layer.geoJson.geometryCollection(earthquakes, {
+    updateTimeDimension: true,
+    updateTimeDimensionMode: 'replace',
+    duration: 'PT1H',
+  }).addTo(myMap);
 
 
-//add legend
-	var legend = L.control({position: 'bottomright'});
+  //add legend
+  var legend = L.control({ position: 'bottomright' });
 
-	legend.onAdd = function (map) {
-		var div = L.DomUtil.create('div', 'info legend'),
-			grades = [0, 1, 2, 3, 4, 5],
-			labels = [];
-			
-		div.innerHTML+='Magnitude<br><hr>'	
+  legend.onAdd = function (map) {
+    var div = L.DomUtil.create('div', 'info legend'),
+      grades = [0, 1, 2, 3, 4, 5, 6, 7, 8],
+      labels = [];
 
-		// loop through our density intervals and generate a label with a colored square for each interval
-		for (var i = 0; i < grades.length; i++) {
-			div.innerHTML +=
-				'<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
-				grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-		}
+    div.innerHTML += 'Magnitude<br><hr>'
 
-		return div;
-	};
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+      div.innerHTML +=
+        '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+        grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
 
-	legend.addTo(myMap);
+    return div;
+  };
+
+  legend.addTo(myMap);
 
 }
